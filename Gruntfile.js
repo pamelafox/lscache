7cd50c1ece3aab5b95d4f004c34616dce601c5cc
Mathieu Triay <mathieu@nightzookeeper.com>
2014-04-15 18:50:17 +0100

    Add grunt-browserify for commonJS tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     jshint: {
-      files: ["lscache.js"],
+      files: ["lscache.js", "./tests/tests.js"],
     },
     uglify: {
       dist: {
@@ -19,13 +19,28 @@ module.exports = function(grunt) {
         tagName: '%VERSION%',
         push: false
       }
+    },
+    browserify: {
+      app: {
+        src: ["./tests/tests.js"],
+        dest: "./tests/tests-cjs.js",
+        options: {
+          shim: {
+            qunit: {
+              path: "./tests/qunit.js",
+              exports: 'qunit'
+            }
+          }
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask("default", ["jshint", "uglify"]);
+  grunt.registerTask("default", ["jshint", "uglify", "browserify"]);
 
 };
